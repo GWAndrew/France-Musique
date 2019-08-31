7afef4f403422cbc8b37b703c6e119ed6c105c6c
GWAndrew <43089870+GWAndrew@users.noreply.github.com>
2019-09-01 00:16:49 +0200

    Add files via upload

---
+++ javascript_index.js
@@ -0,0 +1,8 @@
+
+
+function mouse_follow() {
+    document.getElementById("test").innerHTML = "Hello JavaScript!";
+    
+}
+
+document.getElementById('test').style = 'background-color: red; color: white; font-size: 44px';
